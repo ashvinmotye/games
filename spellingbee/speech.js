e67c56065d32fffc2d78baa5789def99fe635b87
ashvintest <motye13@gmail.com>
2018-04-19 01:10:23 +0400

    Get only English voices

--- spellingbee/speech.js
+++ spellingbee/speech.js
@@ -2,9 +2,23 @@ function speak(text) {
   // create a speechSynthesis reference
   var speech = window.speechSynthesis;
 
+  // get all voices
+  var voices = speech.getVoices();
+
+  // english voices
+  var english = [];
+  for (var i = 0; i < voices.length; i++) {
+    var v = voices[i].voiceURI;
+
+    // check for English language
+    if (v.indexOf('English') !== 0) {
+      english.push(voices[i]);
+    }
+  }
+
   // get a voice
   //taking the first one available at index 0
-  var myVoice = speech.getVoices()[0];
+  var myVoice = english[0];
 
   // creates the content of the speech
   // output holds the text to speak
